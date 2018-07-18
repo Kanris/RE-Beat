6112c53fea5361bfa1d736123e4ccf2f8103f10b
Oleksii Hlukhov <kanris2@gmail.com>
2018-07-18 11:00:02 +0300

    Add cave and Cave entrance script

---
+++ Assets/DisableBackground.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableBackground : MonoBehaviour {
+
+    private bool m_PlayerInCave;
+    private GameObject m_Background;
+
+    private void Start()
+    {
+        InitializeBackground();
+    }
+
+    private void InitializeBackground()
+    {
+        m_Background = GameObject.FindWithTag("BackgroundImage");
+
+        if (m_Background == null)
+        {
+            Debug.LogError("DisableBackground: Can't find gameObject with BackgroundImage tag");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") & !m_PlayerInCave)
+        {
+            m_PlayerInCave = true;
+            ManageBackground();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") & m_PlayerInCave)
+        {
+            m_PlayerInCave = false;
+            ManageBackground();
+        }
+    }
+
+
+    private void ManageBackground()
+    {
+        if (m_Background != null)
+            m_Background.SetActive(!m_PlayerInCave);
+        else
+            Debug.LogError("DisableBackground.ManageBackground: m_Background is not initialized");
+    }
+}
