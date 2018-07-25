9f2801b360f2a77492633a62d48280d9bb781a2c
Alexey Glukhov <kanris2@gmail.com>
2018-07-25 09:09:44 +0300

    Add death of the player due to a fall

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,14 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class Player : MonoBehaviour {
 
     [SerializeField] private float YBoundaries = -20f;
+    [SerializeField] private float YDeath = -3f;
     public PlayerStats playerStats;
+
+    private float m_YPositionBeforeJump;
+    private Animator m_Animator;
 
     private void Start()
     {
         playerStats.Initialize(gameObject);
+
+        InitializeAnimator();
+    }
+
+    private void InitializeAnimator()
+    {
+        m_Animator = GetComponent<Animator>();
+
+        if (m_Animator == null)
+        {
+            Debug.LogError("Player.InitializeAnimator: Can't find Animator component on Gameobject");
+        }
     }
 
     // Update is called once per frame
@@ -19,5 +36,23 @@ public class Player : MonoBehaviour {
         {
             playerStats.TakeDamage(playerStats.MaxHealth);
         }
-	}
+
+        JumpHeightControl();
+
+    }
+
+    private void JumpHeightControl()
+    {
+        if (!m_Animator.GetBool("Ground"))
+        {
+            if (m_YPositionBeforeJump + YDeath >= transform.position.y & !GameMaster.Instance.isPlayerDead)
+            {
+                playerStats.TakeDamage(999);
+            }
+        }
+        else
+        {
+            m_YPositionBeforeJump = transform.position.y;
+        }
+    }
 }
