552e5884bf4bede73b0dc93bd78f885bde4eca47
Alexey Glukhov <kanris2@gmail.com>
2018-08-05 14:05:41 +0300

    Now player can't move further top side of the stairs

--- Assets/Scripts/Environment/Stairs.cs
+++ Assets/Scripts/Environment/Stairs.cs
@@ -6,36 +6,52 @@ using UnityStandardAssets._2D;
 
 public class Stairs : MonoBehaviour {
 
+    private Transform m_StairsTop;
     private Animator m_Animator;
-    private bool isPlayerOnStairs;
     private Rigidbody2D m_Player;
-    [SerializeField] private bool isJumping; 
+
+    [SerializeField] private bool isJumping;
 
     private void Start()
     {
-        isPlayerOnStairs = false;
+        InitializeStairsTop();
+        
+    }
+
+    private void InitializeStairsTop()
+    {
+        if (transform.childCount > 0)
+        {
+            m_StairsTop = transform.GetChild(0).gameObject.transform;
+        }
+        else
+        {
+            Debug.LogError("Stairs.InitializeStairsTop: Can't find top of the stairs");
+        }
+    }
+
+    private void Update()
+    {
+        
     }
 
     private void FixedUpdate()
     {
-        if (isPlayerOnStairs)
+        if (m_Player != null)
         {
-            if (m_Player != null)
+            if (CrossPlatformInputManager.GetAxis("Vertical") != 0f)
             {
-                if (CrossPlatformInputManager.GetAxis("Vertical") != 0f)
-                {
-                    MoveOnStairs();
-                }
-                else if (CrossPlatformInputManager.GetAxis("Horizontal") != 0f & 
-                    CrossPlatformInputManager.GetButton("Jump") & !isJumping)
-                {
-                    isJumping = true;
-                    PlayerOnStairs(false, m_Player.gameObject);
-                }
-                else
-                {
-                    m_Animator.SetBool("IsMovingOnStairs", false);
-                }
+                MoveOnStairs();
+            }
+            else if (CrossPlatformInputManager.GetAxis("Horizontal") != 0f &
+                CrossPlatformInputManager.GetButton("Jump") & !isJumping)
+            {
+                isJumping = true;
+                PlayerOnStairs(false, m_Player.gameObject);
+            }
+            else
+            {
+                m_Animator.SetBool("IsMovingOnStairs", false);
             }
         }
     }
@@ -49,12 +65,15 @@ public class Stairs : MonoBehaviour {
         if (CrossPlatformInputManager.GetAxis("Vertical") < 0f)
             yPos = -yPos;
 
-        m_Player.position += new Vector2(0, yPos);
+        var nextPlayerPosition = m_Player.transform.position.y + yPos;
+
+        if ((m_Player.transform.position.y + yPos) < m_StairsTop.position.y) 
+            m_Player.position += new Vector2(0, yPos);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player") & !isPlayerOnStairs)
+        if (collision.CompareTag("Player"))
         {
             PlayerOnStairs(true, collision.gameObject);
         }
@@ -62,21 +81,20 @@ public class Stairs : MonoBehaviour {
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player") & isPlayerOnStairs)
+        if (collision.CompareTag("Player"))
         {
             PlayerOnStairs(false, collision.gameObject);
 
-            m_Animator = null;
+            isJumping = false;
             m_Player = null;
         }
     }
 
     private void PlayerOnStairs(bool isOnstairs, GameObject collision)
     {
-        isPlayerOnStairs = isOnstairs;
         GetComponentsOnPlayer(collision);
 
-        m_Animator.SetBool("OnStairs", isPlayerOnStairs);
+        m_Animator.SetBool("OnStairs", isOnstairs);
 
         if (isOnstairs)
         {
@@ -87,22 +105,20 @@ public class Stairs : MonoBehaviour {
         else
         {
             m_Player.gravityScale = 3f;
+            m_Player.GetComponent<Platformer2DUserControl>().enabled = true;
 
             if (isJumping)
             {
-                var jumpVector = new Vector2(5f, 10f);
+                /*var jumpVector = new Vector2(5f, 10f);
 
                 if (CrossPlatformInputManager.GetAxis("Horizontal") < 0f)
                 {
                     jumpVector = new Vector2(-5f, 10f);
                 }
 
-                m_Player.velocity = jumpVector;
+                m_Player.velocity = jumpVector;*/
 
-                m_Player.GetComponent<Platformer2DUserControl>().enabled = true;
-
-                m_Player = null;
-                isJumping = false;
+                //m_Player.GetComponent<Platformer2DUserControl>().m_Jump = true;
             }
         }
     }
