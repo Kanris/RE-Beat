42fbe9add8aeb5eb9d0c59c61eac42d36d0d017e
Alexey Glukhov <kanris2@gmail.com>
2018-08-06 15:15:46 +0300

    Add string and box that string can hold

---
+++ Assets/Scripts/Environment/StringTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StringTrigger : MonoBehaviour {
+
+    private Rigidbody2D m_Box;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("GravitySprite"))
+        {
+            m_Box = collision.GetComponent<Rigidbody2D>();
+
+            if (m_Box != null)
+            {
+                m_Box.gravityScale = 0f;
+                m_Box.velocity = Vector2.zero;
+                m_Box.constraints = RigidbodyConstraints2D.FreezeAll;
+            }
+        }
+        else if (collision.CompareTag("PlayerAttackRange"))
+        {
+            if (m_Box != null) m_Box.gravityScale = 3f;
+
+            m_Box.constraints = RigidbodyConstraints2D.FreezePositionX 
+                | RigidbodyConstraints2D.FreezeRotation;
+
+            Destroy(gameObject);
+        }
+    }
+}
