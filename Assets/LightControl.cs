d71e745ff11faca4999cc2cf641723b36815beef
Alexey Glukhov <kanris2@gmail.com>
2018-08-19 20:34:01 +0300

    Add change light intensivity on blue switch

---
+++ Assets/LightControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightControl : MonoBehaviour {
+
+    [SerializeField] private Light LightToControl;
+
+    public void ChangeColorIntensity(int value)
+    {
+        if (LightToControl != null)
+        {
+            LightToControl.intensity = value;
+        }
+    }
+
+}
