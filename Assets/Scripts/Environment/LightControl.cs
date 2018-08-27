6410d19cc9e3674c1d185156f6b9b13223eaff85
Alexey Glukhov <kanris2@gmail.com>
2018-08-27 10:29:47 +0300

    Add light control without animator

--- Assets/Scripts/Environment/LightControl.cs
+++ Assets/Scripts/Environment/LightControl.cs
@@ -5,6 +5,21 @@ using UnityEngine;
 public class LightControl : MonoBehaviour {
 
     [SerializeField] private Light LightToControl;
+    [SerializeField] private bool ControlWithoutAnimator = false;
+    [SerializeField, Range(1, 20)] private float BlinkAfter = 3f;
+    [SerializeField, Range(1, 50)] private float IntensivityIteration = 20f;
+    [SerializeField, Range(1, 10)] private int Iterations = 4;
+
+    private float m_UpdateTimer = 0f;
+
+    private void Start()
+    {
+        if (ControlWithoutAnimator)
+        {
+            m_UpdateTimer = BlinkAfter;
+            LightToControl.intensity = 0f;
+        }
+    }
 
     public void ChangeColorIntensity(int value)
     {
@@ -12,6 +27,32 @@ public class LightControl : MonoBehaviour {
         {
             LightToControl.intensity = value;
         }
+    }
+
+    private void Update()
+    {
+        if (ControlWithoutAnimator)
+        {
+            if (m_UpdateTimer <= Time.time)
+            {
+                m_UpdateTimer = Time.time + BlinkAfter;
+
+                LightToControl.gameObject.SetActive(true);
+                StartCoroutine(LightBlink());
+            }
+        }
+    }
+
+    private IEnumerator LightBlink(int multiplier = 1)
+    {
+        for (int index = 0; index < Iterations; index++)
+        {
+            LightToControl.intensity += IntensivityIteration * multiplier;
+            yield return new WaitForSeconds(0.2f);
+        }
+
+        if (multiplier > 0)
+            StartCoroutine(LightBlink(-1));
     }
 
 }
