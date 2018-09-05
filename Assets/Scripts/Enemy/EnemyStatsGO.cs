a0f0858ef163f434667c778345c3e07280929bbe
Alexey Glukhov <kanris2@gmail.com>
2018-09-05 14:01:35 +0300

    Legendary enemies wouldn't respawn after death

--- Assets/Scripts/Enemy/EnemyStatsGO.cs
+++ Assets/Scripts/Enemy/EnemyStatsGO.cs
@@ -7,15 +7,24 @@ public class EnemyStatsGO : MonoBehaviour {
 
     public Enemy EnemyStats;
 
+    [SerializeField] private bool DontResurect;
+
 	// Use this for initialization
 	void Start () {
 
         InitializeStats();
 
+        if (DontResurect)
+            EnemyStats.OnObjectDeath += SaveState;
     }
 
     public void InitializeStats()
     {
         EnemyStats.Initialize(gameObject, GetComponent<Animator>());
+    }
+
+    public void SaveState()
+    {
+        GameMaster.Instance.SaveState(transform.name, 0, GameMaster.RecreateType.Object);
     }
 }
