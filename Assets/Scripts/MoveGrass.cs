5bb61e679427501160ced58c0fb0629936994a8c
Alexey Glukhov <kanris2@gmail.com>
2018-09-07 13:38:42 +0300

    Add particles on grass hit

--- Assets/Scripts/MoveGrass.cs
+++ Assets/Scripts/MoveGrass.cs
@@ -29,11 +29,33 @@ public class MoveGrass : MonoBehaviour {
         {
             PlayMoveAnimation();
         }
+
+        if (collision.CompareTag("PlayerAttackRange") )
+        {
+            DestroyGrass();
+        }
     }
 
     private void PlayMoveAnimation()
     {
         m_Animator.SetTrigger("Move");
+    }
+
+    private void DestroyGrass()
+    {
+        ShowDestroyParticles();
+        m_Animator.SetTrigger("Hit");
+        transform.position = new Vector3(transform.position.x, transform.position.y - 0.3f);
+    }
+
+    private void ShowDestroyParticles()
+    {
+        var resourceDestroyParticles = Resources.Load("Effects/Destroy grass") as GameObject;
+
+        var resourceDestroyParticlesInstantiate = Instantiate(resourceDestroyParticles);
+        resourceDestroyParticlesInstantiate.transform.position = transform.position;
+
+        Destroy(resourceDestroyParticlesInstantiate, 5f);
     }
 
 }
