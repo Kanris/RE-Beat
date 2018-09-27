0194dc023d713ccf5d539b726e010fb5774dd3d2
Oleksii Hlukhov <kanris2@gmail.com>
2018-09-27 12:20:12 +0300

    Line rope

---
+++ Assets/DrawLine.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(DistanceJoint2D), typeof(LineRenderer))]
+public class DrawLine : MonoBehaviour {
+
+    [SerializeField] private DistanceJoint2D Joint;
+    [SerializeField] private LineRenderer Line;
+
+    private void Start()
+    {
+        var startPoint = Joint.connectedAnchor;
+        Line.SetPosition(0, startPoint);
+
+        /*capsule = gameObject.AddComponent<CapsuleCollider2D>();
+        capsule.isTrigger = true;
+
+        capsule.offset = new Vector2(0, 1);
+        capsule.size = new Vector2(0.2f, 2.8f);*/
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+    
+        Line.SetPosition(1, transform.position);
+
+        /*capsule.transform.position = Joint.connectedAnchor + 
+            ( new Vector2( transform.position.x, transform.position.y ) - Joint.connectedAnchor) / 2;*/
+    }
+}
