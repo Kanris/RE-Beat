2c747c9b6d87367dc5e655391c02bde7ed4c80b1
Alexey Glukhov <kanris2@gmail.com>
2018-10-03 12:08:53 +0300

    Now scripts can hide touch control buttons

---
+++ Assets/MobileButtonsManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MobileButtonsManager : MonoBehaviour {
+
+    [SerializeField] private Image m_Stick;
+    [SerializeField] private Image m_Journal;
+    [SerializeField] private Image m_Pause;
+    [SerializeField] private Image m_Jump;
+    [SerializeField] private Image m_Attack;
+    [SerializeField] private Image m_Submit;
+    [SerializeField] private Image m_Shift;
+
+#region Singleton
+
+    public static MobileButtonsManager Instance;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            if (Instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+
+#endregion
+
+    public void ShowOnlyNeedButtons(bool stick = false, bool journal = false, 
+        bool pause = false, bool jump = false, bool attack = false, 
+        bool submit = false, bool shift = false)
+    {
+        m_Stick.enabled = stick;
+        m_Journal.enabled = journal;
+        m_Pause.enabled = pause;
+        m_Jump.enabled = jump;
+        m_Attack.enabled = attack;
+        m_Submit.enabled = submit;
+        m_Shift.enabled = shift;
+    }
+
+    public void HideOnlyNeedButtons(bool stick = true, bool journal = true,
+        bool pause = true, bool jump = true, bool attack = true,
+        bool submit = true, bool shift = true)
+    {
+        m_Stick.enabled = stick;
+        m_Journal.enabled = journal;
+        m_Pause.enabled = pause;
+        m_Jump.enabled = jump;
+        m_Attack.enabled = attack;
+        m_Submit.enabled = submit;
+        m_Shift.enabled = shift;
+    }
+}
