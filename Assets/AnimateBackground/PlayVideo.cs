d0f6e435907a4cdf4066d68e2bed3533e75ba539
Alexey Glukhov <kanris2@gmail.com>
2018-10-26 21:58:00 +0300

    Add: animate background for start and load screens

---
+++ Assets/AnimateBackground/PlayVideo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class PlayVideo : MonoBehaviour {
+
+    [SerializeField] private VideoPlayer m_VideoToPlay;
+
+	// Use this for initialization
+	void Start () {
+
+        StartCoroutine(PlayBackground());
+    }
+
+    private IEnumerator PlayBackground()
+    {
+        m_VideoToPlay.Prepare();
+
+        while (!m_VideoToPlay.isPrepared)
+        {
+            yield return new WaitForSeconds(0.05f);
+        }
+
+        GetComponent<RawImage>().texture = m_VideoToPlay.texture;
+        m_VideoToPlay.Play();
+    }
+}
