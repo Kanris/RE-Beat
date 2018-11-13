f200fc39e661d6a227f206e3c56c398c79cf498e
Oleksii Hlukhov <kanris2@gmail.com>
2018-11-13 10:55:30 +0200

    Fix: drone kamikaze now don't leave trail effect when fall off to start destroy sequence

--- Assets/Scripts/Enemy/Types/General/Drone/DroneKamikaze.cs
+++ Assets/Scripts/Enemy/Types/General/Drone/DroneKamikaze.cs
@@ -101,6 +101,8 @@ public class DroneKamikaze : MonoBehaviour
                 m_IsDestroying = true;
                 m_Rigidbody.sharedMaterial = null;
                 m_Rigidbody.gravityScale = 3f;
+
+                Destroy(GetComponent<TrailRenderer>());
             }
             else
                 PlayTriggerAnimation("Hit");
