635ea608987bd89e22b0e4c866af04e8703569ed
Alexey Glukhov <kanris2@gmail.com>
2018-12-04 20:43:08 +0200

    Add: companion animations - InTunnel and FromTunnel

--- Assets/Tunnel.cs
+++ Assets/Tunnel.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Tunnel : MonoBehaviour {
 
+    [SerializeField] AnimationClip m_InTunnelAnimation;
+
     private Transform m_SpawnOnExit;
     private static Transform m_Companion;
 
@@ -12,17 +14,23 @@ public class Tunnel : MonoBehaviour {
         m_SpawnOnExit = transform.GetChild(0);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private IEnumerator OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Companion"))
         {
             m_Companion = collision.transform;
-            
+
+            m_Companion.GetComponent<Animator>().SetTrigger("InTunnel");
+
+            yield return new WaitForSeconds(m_InTunnelAnimation.length);
+
+            yield return null;
+
             collision.gameObject.SetActive(false);
         }
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
+    private IEnumerator OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
@@ -33,6 +41,12 @@ public class Tunnel : MonoBehaviour {
                     m_Companion.position = m_SpawnOnExit.position;
                     m_Companion.gameObject.SetActive(true);
 
+                    m_Companion.GetComponent<Animator>().SetTrigger("FromTunnel");
+                    m_Companion.GetComponent<Companion>().enabled = false;
+
+                    yield return new WaitForSeconds(m_InTunnelAnimation.length);
+
+                    m_Companion.GetComponent<Companion>().enabled = true;
                     m_Companion.GetComponent<Companion>().SetTunnel(transform);
 
                     m_Companion = null;
